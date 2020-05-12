4c75e5b05b6aff20d39569ae8af115de0aad167a
Antoine Lever <alever@omniis.com>
2020-05-12 09:21:54 +0100

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:port_viewer/screens/portfolios_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Portfolio Viewer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PortfoliosScreen(),
+    );
+  }
+}
---
+++ lib/screens/portfolios_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PortfoliosScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Portfolios')),
+      body: Center(child: Text('Here\'s hoping!'),),
+    );
+  }
+}
